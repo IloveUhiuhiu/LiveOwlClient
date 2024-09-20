e9f6db319bd33bfdb6edadcfe3c7d0f90fa2cb8b
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-09-20 18:38:10 +0700

    test-home-fxml

--- src/main/java/com/client/liveowl/JavaFxApplication.java
+++ src/main/java/com/client/liveowl/JavaFxApplication.java
@@ -2,16 +2,22 @@ package com.client.liveowl;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class JavaFxApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane helloPane = new Pane(new Label("Hello JavaFx"));
-        primaryStage.setScene(new Scene(helloPane, 200, 100));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
+        // Thiết lập biểu tượng ứng dụng
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        HBox root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("LiveOwl Application");
         primaryStage.show();
     }
 }
